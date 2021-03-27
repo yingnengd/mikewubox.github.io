5ed9fc1d9b298dbca7a57e7e406368708abcdb85
yingnengd <80525738+yingnengd@users.noreply.github.com>
2021-03-27 07:27:19 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: FZS
+ * Time: 2021/03/27  
+ */
+class freeApi
+{
+    private $apiUrl;
+
+    public function __construct()
+    {
+        $this->apiUrl = 'https://way.jd.com/JDAI/intelligent_creation?sku=100006079301&appkey=15cdd5b196a4961065ca669c2224e87f';
+    }
+
+    /**
+     * 获取结果
+     * @return array
+     */
+    public function getResult()
+    {
+        return file_get_contents($this->apiUrl);
+    }
+}
